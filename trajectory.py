97ed988d026c3c1a1a786298d2eb9c695804d974
Manish Kumar Singh <coolmanish5255@gmail.com>
2022-04-05 12:35:40 +0530

    First commit 3 Problem

---
+++ trajectory.py
@@ -0,0 +1,9 @@
+import math
+x=float(input('Enter x coordinate value : '))
+y0=float(input('Enter the initial position y coordinate : '))
+angle=float(input('Enter angle value made with x coordinate : '))
+g=9.8
+v=30*(18/5)
+result=(x*math.tan(angle))-((g*math.pow(x,2))/(2*math.pow(v,2)*math.pow(math.cos(angle),2)))+y0
+
+print("result : "+str(result))
